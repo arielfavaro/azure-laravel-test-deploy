df92cea2c22aaad3522d2dbb55b81d750008ea1a
Ariel Favaro <arieltainan@gmail.com>
2021-10-14 14:18:48 -0300

    updated startup script

--- startup.sh
+++ startup.sh
@@ -1,6 +1,10 @@
 #!/bin/bash
 echo "Replacing nginx configuration for serving PHP 8.0-based applications"
-cp /home/site/repository/deployment/azure-app-service/nginx.conf /etc/nginx/sites-available/default
+# cp /home/site/repository/deployment/azure-app-service/nginx.conf /etc/nginx/sites-available/default
+
+# teste temporario
+# TODO verificar diretorios /home/site/deployments e /home/site/repository e quando sao usados
+cp /home/site/wwwroot/nginx.conf /etc/nginx/sites-available/default
 
 echo "Reloading nginx to apply new configuration"
 service nginx reload
